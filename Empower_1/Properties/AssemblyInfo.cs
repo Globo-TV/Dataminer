5b265e4746f0a35923d9dab0360775f400eba21f
yrijo <yrijo@WPA0866G5XF3N>
2023-05-30 15:55:02 -0300

    Adicionar arquivos de projeto.

---
+++ Empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("76BD75B4-8B35-43DB-A072-89B1EB7048E0")]
+[assembly: AssemblyVersion("1.0.0.0")]
